f08a0fbe1be501dc80a0c653c501c1a245bf616c
avatar92 <adelbensalem1992@gmail.com>
2019-06-14 12:24:02 +0200

    Updates

---
+++ precache-manifest.fbcd9ac4b74a984cc169513964745f78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d964c4a2780569af7abec3ae328cba26",
+    "url": "/react-docplanner-clone/index.html"
+  },
+  {
+    "revision": "e9f2ca771bb27ef4d11e",
+    "url": "/react-docplanner-clone/static/css/main.3c76262e.chunk.css"
+  },
+  {
+    "revision": "025bfe60225947dfd651",
+    "url": "/react-docplanner-clone/static/js/2.3beab2da.chunk.js"
+  },
+  {
+    "revision": "e9f2ca771bb27ef4d11e",
+    "url": "/react-docplanner-clone/static/js/main.75e48193.chunk.js"
+  },
+  {
+    "revision": "6ae9aa1d9a5888f2c4a0",
+    "url": "/react-docplanner-clone/static/js/runtime~main.edbd503d.js"
+  }
+]);
